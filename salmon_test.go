799cca8c8f67f999d56981e28e6ddd8eb08f87a8
zhiming.sun <zhiming.sun@qq.com>
2023-10-12 16:27:54 +0800

    up

--- salmon_test.go
+++ salmon_test.go
@@ -2,17 +2,18 @@ package salmon
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 )
 
 func TestSalmon(t *testing.T) {
 	p, _ := NewPool(10, func(v interface{}) {
-		time.Sleep(3 * time.Second)
+		time.Sleep(time.Duration(rand.Intn(2)+1) * time.Second)
 		fmt.Println(v)
 	})
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 100; i++ {
 		p.Invoke(i)
 	}
 
